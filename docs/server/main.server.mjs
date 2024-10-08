9b83d2665250fee4f73562be17ab49d4a1d2daab
Sorina Ioja <sorina_ioja@yahoo.com>
2024-10-08 15:39:39 +0300

    CV changed and about section

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VVUOROPA.mjs";import"./chunk-LLONHKMV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-IQV5UHOH.mjs";import"./chunk-LLONHKMV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
